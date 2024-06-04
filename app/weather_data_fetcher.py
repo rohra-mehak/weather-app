5f23ee3bee4582a22529bc08ddf735170d511622
rohra-mehak <rohramehak@gmail.com>
2024-06-05 00:17:31 +0200

    -- weather data fetcher docs

--- app/weather_data_fetcher.py
+++ app/weather_data_fetcher.py
@@ -4,21 +4,46 @@ import os
 import pandas as pd
 
 class WeatherDataFetcher:
+    """
+    A class to fetch weather data for a specific location.
+
+    Attributes:
+        location (str): The location for which weather data is fetched.
+        weather_data: The fetched weather data.
+        daily_forecasts: The daily weather forecasts.
+        hourly_forecasts: The aggregated hourly weather forecasts for each day.
+    """
+
     def __init__(self, location='New York'):
+        """
+        Initializes WeatherDataFetcher with the specified location.
+
+        Args:
+            location (str, optional): The location for which weather data is fetched. Defaults to 'New York'.
+        """
         self.location = location
         self.weather_data = None
         self.daily_forecasts = None
         self.hourly_forecasts = None
-        
 
     async def fetch_weather(self):
+        """
+        Fetches weather data for the specified location using the python_weather API.
+        """
         async with python_weather.Client() as client:
             self.weather_data = await client.get(location=self.location)
             self.daily_forecasts = [day for day in self.weather_data.daily_forecasts]
             self.hourly_forecasts = {day.date: [hourly_data for hourly_data in day.hourly_forecasts] for day in self.daily_forecasts}
-            
-
+    
+    
     def get_daily_forecasts(self):
+        """
+        Process the weather information associated with the daily
+        forecasts into a pandas DataFrame
+
+        Returns:
+            pandas.DataFrame or None: DataFrame containing daily weather forecasts.
+        """
         if self.weather_data is not None:
             forecasts = {
                 "Date": [],
@@ -41,6 +66,14 @@ class WeatherDataFetcher:
         return None
     
     def get_hourly_forecasts(self):
+        """
+        Process and aggregates the weather information associated 
+        with the hourly forecasts of each day into a 
+        pandas DataFrame
+
+        Returns:
+            pandas.DataFrame or None: DataFrame containing hourly weather forecasts.
+        """
         if self.weather_data is not None:
             hourly_forecasts = {
                 "Date": [],
@@ -50,7 +83,6 @@ class WeatherDataFetcher:
                 "Temperature": [], 
                 "Description": [],
                 "Wind_Speed": [],
-
             }
 
             for date, hourly_data in self.hourly_forecasts.items():
@@ -63,17 +95,21 @@ class WeatherDataFetcher:
                     hourly_forecasts["Description"].append(hour.description)
                     hourly_forecasts["Wind_Speed"].append(hour.wind_speed)
             
-
             df = pd.DataFrame(hourly_forecasts)
             df['DateTime'] = pd.to_datetime(df['Date'].astype(str) + ' ' + df['Hour'].astype(str))
-            # df.set_index('DateTime', inplace=True)
- 
+           
             return df
         return None
 
-
-
     async def get_weather(self):
+        """
+        Method to fetch weather and return current temp, description, 
+        feels like, wind speed, visibility, processed daily 
+        and hourly forecasts in a dictionary. 
+
+        Returns:
+            dict: Dictionary containing weather data.
+        """
         await self.fetch_weather()
         return {
             "temperature": self.weather_data.temperature,
@@ -86,9 +122,10 @@ class WeatherDataFetcher:
         }
 
 if __name__ == '__main__':
+# Example usage
     if os.name == 'nt':
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
-    location = input("enter location name")
+    location = input("Enter location name: ")
     weather = WeatherDataFetcher(location)
     weather_data = asyncio.run(weather.get_weather())
     print(weather_data)
