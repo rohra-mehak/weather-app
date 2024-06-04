5238d2da2aed1ec39bc6ddc75a2009a518a7296c
rohra-mehak <rohramehak@gmail.com>
2024-06-05 01:27:55 +0200

    -- code formatting
    -- code formatting and restructuring

--- app/weather_data_fetcher.py
+++ app/weather_data_fetcher.py
@@ -3,6 +3,7 @@ import asyncio
 import os
 import pandas as pd
 
+
 class WeatherDataFetcher:
     """
     A class to fetch weather data for a specific location.
@@ -14,12 +15,12 @@ class WeatherDataFetcher:
         hourly_forecasts: The aggregated hourly weather forecasts for each day.
     """
 
-    def __init__(self, location='New York'):
+    def __init__(self, location="New York"):
         """
         Initializes WeatherDataFetcher with the specified location.
 
         Args:
-            location (str, optional): The location for which weather data 
+            location (str, optional): The location for which weather data
             is fetched. Defaults to 'New York'.
         """
         self.location = location
@@ -34,8 +35,10 @@ class WeatherDataFetcher:
         async with python_weather.Client() as client:
             self.weather_data = await client.get(location=self.location)
             self.daily_forecasts = [day for day in self.weather_data.daily_forecasts]
-            self.hourly_forecasts = {day.date: [hourly_data for hourly_data in day.hourly_forecasts] for day in self.daily_forecasts}
-    
+            self.hourly_forecasts = {
+                day.date: [hourly_data for hourly_data in day.hourly_forecasts]
+                for day in self.daily_forecasts
+            }
 
     def get_daily_forecasts(self):
         """
@@ -45,68 +48,72 @@ class WeatherDataFetcher:
         Returns:
             pandas.DataFrame or None: DataFrame containing daily weather forecasts.
         """
-        if self.weather_data is not None:
-            forecasts = {
-                "Date": [],
-                "Highest Temperature": [],
-                "Lowest Temperature": [],
-                "Temperature": []
-            }
+        if self.weather_data is None:
+            return
+        forecasts = {
+            "Date": [],
+            "Highest Temperature": [],
+            "Lowest Temperature": [],
+            "Temperature": [],
+        }
 
-            for day in self.daily_forecasts:
-                forecasts["Date"].append(day.date)
-                forecasts["Highest Temperature"].append(day.highest_temperature)
-                forecasts["Lowest Temperature"].append(day.lowest_temperature)
-                forecasts["Temperature"].append(day.temperature)
+        for day in self.daily_forecasts:
+            forecasts["Date"].append(day.date)
+            forecasts["Highest Temperature"].append(day.highest_temperature)
+            forecasts["Lowest Temperature"].append(day.lowest_temperature)
+            forecasts["Temperature"].append(day.temperature)
 
-            df = pd.DataFrame(forecasts)
-            temperature_df = df.melt(id_vars=["Date"], var_name="Temperature Variant", value_name="value")
-            temperature_df['Date'] = pd.to_datetime(temperature_df['Date'])
+        df = pd.DataFrame(forecasts)
+        temperature_df = df.melt(
+            id_vars=["Date"], var_name="Temperature Variant", value_name="value"
+        )
+        temperature_df["Date"] = pd.to_datetime(temperature_df["Date"])
 
-            return temperature_df
-        return None
-    
+        return temperature_df
+
     def get_hourly_forecasts(self):
         """
-        Process and aggregates the weather information associated 
-        with the hourly forecasts of each day into a 
+        Process and aggregates the weather information associated
+        with the hourly forecasts of each day into a
         pandas DataFrame
 
         Returns:
             pandas.DataFrame or None: DataFrame containing hourly weather forecasts.
         """
-        if self.weather_data is not None:
-            hourly_forecasts = {
-                "Date": [],
-                "Hour": [],
-                "Feels_Like": [],
-                "Humidity": [],
-                "Temperature": [], 
-                "Description": [],
-                "Wind_Speed": [],
-            }
+        if self.weather_data is None:
+            return
+        hourly_forecasts = {
+            "Date": [],
+            "Hour": [],
+            "Feels_Like": [],
+            "Humidity": [],
+            "Temperature": [],
+            "Description": [],
+            "Wind_Speed": [],
+        }
 
-            for date, hourly_data in self.hourly_forecasts.items():
-                for hour in hourly_data:
-                    hourly_forecasts["Date"].append(date)
-                    hourly_forecasts["Hour"].append(hour.time)
-                    hourly_forecasts["Feels_Like"].append(hour.feels_like)
-                    hourly_forecasts["Humidity"].append(hour.humidity)
-                    hourly_forecasts["Temperature"].append(hour.temperature)
-                    hourly_forecasts["Description"].append(hour.description)
-                    hourly_forecasts["Wind_Speed"].append(hour.wind_speed)
-            
-            df = pd.DataFrame(hourly_forecasts)
-            df['DateTime'] = pd.to_datetime(df['Date'].astype(str) + ' ' + df['Hour'].astype(str))
-           
-            return df
-        return None
+        for date, hourly_data in self.hourly_forecasts.items():
+            for hour in hourly_data:
+                hourly_forecasts["Date"].append(date)
+                hourly_forecasts["Hour"].append(hour.time)
+                hourly_forecasts["Feels_Like"].append(hour.feels_like)
+                hourly_forecasts["Humidity"].append(hour.humidity)
+                hourly_forecasts["Temperature"].append(hour.temperature)
+                hourly_forecasts["Description"].append(hour.description)
+                hourly_forecasts["Wind_Speed"].append(hour.wind_speed)
+
+        df = pd.DataFrame(hourly_forecasts)
+        df["DateTime"] = pd.to_datetime(
+            df["Date"].astype(str) + " " + df["Hour"].astype(str)
+        )
+
+        return df
 
     async def get_weather(self):
         """
-        Method to fetch weather and return current temp, description, 
-        feels like, wind speed, visibility, processed daily 
-        and hourly forecasts in a dictionary. 
+        Method to fetch weather and return current temp, description,
+        feels like, wind speed, visibility, processed daily
+        and hourly forecasts in a dictionary.
 
         Returns:
             dict: Dictionary containing weather data.
@@ -117,14 +124,15 @@ class WeatherDataFetcher:
             "description": self.weather_data.description,
             "feels_like": self.weather_data.feels_like,
             "wind_speed": self.weather_data.wind_speed,
-            "visibility": self.weather_data.visibility, 
+            "visibility": self.weather_data.visibility,
             "daily_forecasts": self.get_daily_forecasts(),
-            "hourly_forecasts" : self.get_hourly_forecasts()
+            "hourly_forecasts": self.get_hourly_forecasts(),
         }
 
-if __name__ == '__main__':
-# Example usage
-    if os.name == 'nt':
+
+if __name__ == "__main__":
+    # Example usage
+    if os.name == "nt":
         asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
     location = input("Enter location name: ")
     weather = WeatherDataFetcher(location)
--- app/weather_location_query_handler.py
+++ app/weather_location_query_handler.py
@@ -3,6 +3,7 @@ import spacy
 # Load the medium-sized English model provided by spaCy
 nlp = spacy.load("en_core_web_md")
 
+
 class WeatherLocationQueryHandler:
     def __init__(self, query) -> None:
         """
@@ -12,25 +13,26 @@ class WeatherLocationQueryHandler:
         query (str): The user query to be processed.
         """
         # Define the weather-related keywords using spaCy
-        self.weather_nlp = nlp("Weather current temperature conditions in a city ? sunny windy raining cloudy")
+        self.weather_nlp = nlp(
+            "Weather current temperature conditions in a city ? sunny windy raining cloudy"
+        )
         self.query = query
-    
+
     def get_response(self):
         """
-        process user query to check its relatedness with weather 
-        and extract location 
-       
+        process user query to check its relatedness with weather
+        and extract location
+
         Returns:
         str: Response to the user query.
         """
         statement = nlp(self.query)
         min_similarity = 0.5
-        
-        # Check if the user query is related to weather conditions in 
-        # a location
+
+        # Check if the user query is related to weather conditions in a location
         if not self.weather_nlp.similarity(statement) >= min_similarity:
             return "Sorry I don't understand that. Please rephrase your statement including the desired location."
-            
+
         # Extract any geographical entities mentioned in the query
         for ent in statement.ents:
             if not ent.label_ == "GPE":
@@ -39,9 +41,9 @@ class WeatherLocationQueryHandler:
             if location is None:
                 return "Something went wrong. Please try again"
             return location, f"Weather Information for {location}"
-        
+
         return "Please include a location to check for weather"
-                
+
 
 if __name__ == "__main__":
     # Example usage
